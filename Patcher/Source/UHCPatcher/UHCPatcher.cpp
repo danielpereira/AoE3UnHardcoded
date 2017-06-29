b45c1f152917473ed578f9d88eef11ef07056774
Daniel Pereira <danielpereira1918@hotmail.com>
2017-06-29 15:48:57 -0300

    Commiting latest GUI Patcher source

---
+++ Patcher/Source/UHCPatcher/UHCPatcher.cpp
@@ -0,0 +1,414 @@
+// UHCPatcher.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "UHCPatcher.h"
+
+#define MAX_LOADSTRING 100
+
+#define UHC_NAME L"UHC Patcher"
+#define UHC_RESNAME L"BIN"
+#define UHC_LIB_NAME L"UHC.DLL"
+#define UHC_HEADER 0x31434855
+#define UHC2_HEADER 0x32434855
+
+#define CFG_PATH_MAX 21
+
+// Global Variables:
+WCHAR exePath[MAX_PATH];
+WCHAR exeBackupPath[MAX_PATH] = L"";
+BOOL backupExists = FALSE;
+WCHAR cfgFileRelPath[CFG_PATH_MAX] = L"Startup\\uhc.cfg";
+DWORD patchSettings = 0;
+
+// Patch files must be named as the patch name
+// They will be read from the same directory of this EXE
+// File Specs (.uhc)
+// header (4 bytes) UHC_HEADER
+// data count (4 bytes)
+// data 0 offset (4 bytes)
+// data 0 length (4 bytes)
+// data 0 [bytes]
+// data 1...
+// ...
+
+typedef enum UHC_PATCH_RESULT {
+	UHC_PATCH_NONE,
+	UHC_PATCH_FAILED,
+	UHC_PATCH_SUCCESS
+} UHC_PATCH_RESULT;
+
+BOOL ReadCFGPathFromExe(WCHAR* cfgFilePath) {
+	WCHAR tmpCfgPath[CFG_PATH_MAX];
+	DWORD bytesRead;
+
+	HANDLE exeHandle = CreateFile(exePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if (exeHandle != INVALID_HANDLE_VALUE) {
+		size_t pcch;
+
+		if (SetFilePointer(exeHandle, 0x00865083, NULL, FILE_BEGIN) != INVALID_SET_FILE_POINTER) {
+			ReadFile(exeHandle, tmpCfgPath, CFG_PATH_MAX * 2, &bytesRead, NULL);
+
+			if (SUCCEEDED(StringCchLength(tmpCfgPath, CFG_PATH_MAX, &pcch)) && pcch) {
+				lstrcpy(cfgFilePath, tmpCfgPath);
+				return TRUE;
+			}
+			else {
+				if (SetFilePointer(exeHandle, 0x008AA0B0, NULL, FILE_BEGIN) != INVALID_SET_FILE_POINTER) {
+					ReadFile(exeHandle, tmpCfgPath, CFG_PATH_MAX * 2, &bytesRead, NULL);
+
+					if (SUCCEEDED(StringCchLength(tmpCfgPath, CFG_PATH_MAX, &pcch)) && pcch) {
+						lstrcpy(cfgFilePath, tmpCfgPath);
+						return TRUE;
+					}
+				}
+			}
+		}
+	}
+
+	return FALSE;
+}
+
+BOOL IsAValidExecutable(WCHAR* lpExePath, BOOL* backupExists) {
+	DWORD dwAttr = GetFileAttributesW(lpExePath);
+
+	if (dwAttr != INVALID_FILE_ATTRIBUTES && !(dwAttr & FILE_ATTRIBUTE_DIRECTORY)) {
+		for (int i = lstrlenW(lpExePath) - 1; i >= 0; --i) {
+			if (lpExePath[i] == '.') {
+				if (!lstrcmpiW(&lpExePath[i], L".exe")) {
+					HANDLE exeHandle = CreateFileW(lpExePath, 0, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+					if (GetFileSize(exeHandle, NULL) != 11598648) {
+						CloseHandle(exeHandle);
+
+						if (!(MessageBox(NULL, L"The supplied executable doesn't seem to be a valid AoE3 TAD executable\nDo you want to proceed with patching this file, anyway?", UHC_NAME, MB_ICONQUESTION | MB_YESNO) == IDYES))
+							return FALSE;
+					}
+					
+					lstrcpy(exeBackupPath, lpExePath);
+					exeBackupPath[i] = '\0';
+					lstrcatW(exeBackupPath, L".backupuhc");
+
+					DWORD dwAttrBackup = GetFileAttributesW(exeBackupPath);
+
+					(*backupExists) = (dwAttr != INVALID_FILE_ATTRIBUTES && !(dwAttr & FILE_ATTRIBUTE_DIRECTORY)) ? TRUE : FALSE;
+
+					return TRUE;
+
+				}
+				else {
+					MessageBox(NULL, L"The supplied file is not a valid executable file.", L"Error", MB_ICONERROR | MB_OK);
+					return FALSE;
+				}
+			}
+		}
+	}
+
+	MessageBox(NULL, L"The supplied file is not a valid executable file.", L"Error", MB_ICONERROR | MB_OK);
+	return FALSE;
+}
+
+BOOL SelectExecutableFile(HWND parentWnd, LPWSTR fileName, BOOL* backupExists) {
+	bool fileChosen = FALSE;
+	OPENFILENAME ofn;
+
+	ZeroMemory(&ofn, sizeof(OPENFILENAME));
+	ofn.lStructSize = sizeof(OPENFILENAME);
+	ofn.hwndOwner = parentWnd;
+	ofn.lpstrFile = fileName;
+	fileName[0] = '\0';
+	ofn.nMaxFile = MAX_PATH;
+	ofn.lpstrFilter = L"Executable files\0*.EXE\0";
+	ofn.nFilterIndex = 1;
+	ofn.lpstrFileTitle = NULL;
+	ofn.nMaxFileTitle = 0;
+	ofn.lpstrInitialDir = NULL;
+
+	while (!fileChosen) {
+		if (GetOpenFileName(&ofn)) {
+			if (IsAValidExecutable(ofn.lpstrFile, backupExists)) {
+				fileChosen = TRUE;
+				return TRUE;
+			}
+			else {
+				fileName[0] = '\0';
+			}
+		}
+		else {
+			fileChosen = TRUE;
+		}
+	}
+
+	return FALSE;
+}
+
+/*BOOL UHCSaveLibs() {
+	static DWORD UHC_LIB_IDS[UHC_LIB_COUNT] = { 202, 203 };
+	static LPWSTR UHC_LIB_NAMES[UHC_LIB_COUNT] = { L"uhc_wrapper.dll", L"uhc.dll" };
+
+	for (DWORD i = 0; i < UHC_LIB_COUNT; i++) {
+		HANDLE hLib = CreateFileW(UHC_LIB_NAMES[i], (GENERIC_READ | GENERIC_WRITE), FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+		if (!hLib)
+			return FALSE;
+
+		HRSRC resHandle = FindResource(NULL, MAKEINTRESOURCE(UHC_LIB_IDS[i]), UHC_RESNAME);
+		HGLOBAL resData = LoadResource(NULL, resHandle);
+		DWORD resSize = SizeofResource(NULL, resHandle);
+
+		DWORD bytesWritten = 0;
+
+		if (!(WriteFile(hLib, resData, resSize, &bytesWritten, NULL) && (bytesWritten == resSize)))
+			return FALSE;
+
+		CloseHandle(hLib);
+
+	}
+
+	return TRUE;
+}*/
+
+BOOL UHCPatch(HANDLE hFile, HANDLE hPatchFile) {
+	static HANDLE hHeap = GetProcessHeap();
+	DWORD dwBytes;
+
+	DWORD dwHeader;
+	ReadFile(hPatchFile, &dwHeader, 4, &dwBytes, NULL);
+
+	if (dwHeader != UHC_HEADER) {
+		return FALSE;
+	}
+
+	DWORD dwDataCount;
+	ReadFile(hPatchFile, &dwDataCount, 4, &dwBytes, NULL);
+
+	for (DWORD i = 0; i < dwDataCount; ++i) {
+		DWORD dwOffset, dwLength;
+		LPSTR lpData;
+
+		ReadFile(hPatchFile, &dwOffset, 4, &dwBytes, NULL);
+		ReadFile(hPatchFile, &dwLength, 4, &dwBytes, NULL);
+
+		lpData = (LPSTR)HeapAlloc(hHeap, 0, dwLength);
+		ReadFile(hPatchFile, lpData, dwLength, &dwBytes, NULL);
+
+		if (SetFilePointer(hFile, dwOffset, NULL, FILE_BEGIN) == INVALID_SET_FILE_POINTER)
+			return FALSE;
+
+		WriteFile(hFile, lpData, dwLength, &dwBytes, NULL);
+
+		HeapFree(hHeap, 0, lpData);
+	}
+
+	return TRUE;
+}
+
+UHC_PATCH_RESULT PatchExecutable(HWND hwndDlg) {
+	if (!backupExists && !(CopyFileW(exePath, exeBackupPath, FALSE))) {
+		if (MessageBoxW(hwndDlg, L"The patcher has failed to backup the current executable.\nContinue patching anyway?", L"Error", MB_ICONWARNING | MB_YESNO) != IDYES) {
+			return UHC_PATCH_NONE;
+		}
+	}
+
+	WCHAR dirBuffer[MAX_PATH];
+	WCHAR tmpFile[MAX_PATH];
+
+	GetTempPath(MAX_PATH, dirBuffer);
+	GetTempFileNameW(dirBuffer, L"UHC", 0, tmpFile);
+	HANDLE hTmpPatchFile = CreateFileW(tmpFile, GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	HRSRC resHandle = FindResource(NULL, MAKEINTRESOURCE(IDR_PATCH), UHC_RESNAME);
+	HGLOBAL resData = LoadResource(NULL, resHandle);
+	DWORD resSize = SizeofResource(NULL, resHandle);
+
+	DWORD bytesWritten = 0;
+	WriteFile(hTmpPatchFile, resData, resSize, &bytesWritten, NULL);
+	
+	HANDLE hExeFile = CreateFileW(exePath, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if (hExeFile == INVALID_HANDLE_VALUE) {
+		MessageBoxW(hwndDlg, L"Failed to open the supplied executable file.", L"Error", MB_ICONERROR | MB_OK);
+		CloseHandle(hTmpPatchFile);
+		DeleteFileW(tmpFile);
+		return UHC_PATCH_NONE;
+	}
+
+	if (!UHCPatch(hExeFile, hTmpPatchFile)) {
+		CloseHandle(hExeFile);
+		CloseHandle(hTmpPatchFile);
+		DeleteFileW(tmpFile);
+		return UHC_PATCH_FAILED;
+	}
+
+	if (SetFilePointer(hExeFile, 0x00865083, NULL, FILE_BEGIN) != INVALID_SET_FILE_POINTER)
+		WriteFile(hExeFile, cfgFileRelPath, (lstrlen(cfgFileRelPath) + 1) * 2, &bytesWritten, NULL);
+
+	CloseHandle(hExeFile);
+	CloseHandle(hTmpPatchFile);
+	DeleteFileW(tmpFile);
+
+	Config* cfgFile = new Config;
+
+	lstrcpyW(dirBuffer, exePath);
+
+	int lastSlashIndex;
+
+	for (int i = lstrlen(dirBuffer) - 1, stop = 0; i <= 0 && !stop; i++) {
+		if (dirBuffer[i] == '\\') {
+			lastSlashIndex = i;
+			dirBuffer[i + 1] == '\0';
+			lstrcatW(dirBuffer, cfgFileRelPath);
+		}
+	}
+
+	cfgFile->Parse(dirBuffer);
+	cfgFile->ProcessData();
+	
+	if (!cfgFile->WriteToFile(dirBuffer, patchSettings))
+		MessageBoxW(hwndDlg, L"The patcher has failed to write to the supplied configuration file\n\nIn order to apply the intended patch settings,\n you will have to edit it manually.\n\nRefer to the official UHC Configuration File documentation for more information.", UHC_NAME, MB_ICONINFORMATION | MB_OK);
+
+	delete cfgFile;
+
+	dirBuffer[lastSlashIndex + 1] = '\0';
+	lstrcatW(dirBuffer, UHC_LIB_NAME);
+
+	HANDLE hLib = CreateFileW(dirBuffer, (GENERIC_READ | GENERIC_WRITE), FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	resHandle = FindResource(NULL, MAKEINTRESOURCE(IDR_UHC), UHC_RESNAME);
+	resData = LoadResource(NULL, resHandle);
+	resSize = SizeofResource(NULL, resHandle);
+
+	if (!(WriteFile(hLib, resData, resSize, &bytesWritten, NULL) && (bytesWritten == resSize))) {
+		MessageBoxW(hwndDlg, L"The patcher has failed to place the main DLL at the path of the supplied executable.\nYou'll have to manually download it and place it.", UHC_NAME, MB_ICONERROR | MB_OK);
+	}
+
+	return UHC_PATCH_SUCCESS;
+}
+
+INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	switch (uMsg) {
+		case WM_INITDIALOG: {
+			SendDlgItemMessage(hwndDlg, IDC_CFGNAME, EM_SETLIMITTEXT, 20, NULL);
+
+			if ((BOOL)lParam && IsAValidExecutable(exePath, &backupExists)) {
+				SetDlgItemText(hwndDlg, IDC_EXEPATH, exePath);
+				EnableWindow(GetDlgItem(hwndDlg, IDC_APPLY), TRUE);
+				ReadCFGPathFromExe(cfgFileRelPath);
+
+				if (backupExists)
+					EnableWindow(GetDlgItem(hwndDlg, IDC_RESTORE), TRUE);
+			}
+
+			SetDlgItemText(hwndDlg, IDC_CFGNAME, cfgFileRelPath);
+
+
+			return TRUE;
+		}
+		case WM_CLOSE:
+			EndDialog(hwndDlg, 0);
+			break;
+		case WM_COMMAND:
+			switch (LOWORD(wParam)) {
+				case IDC_OPEN: {
+					WCHAR tmpFileName[MAX_PATH];
+
+					if (SelectExecutableFile(hwndDlg, tmpFileName, &backupExists)) {
+						lstrcpy(exePath, tmpFileName);
+						SetDlgItemText(hwndDlg, IDC_EXEPATH, exePath);
+						EnableWindow(GetDlgItem(hwndDlg, IDC_APPLY), TRUE);
+
+						if (ReadCFGPathFromExe(cfgFileRelPath))
+							SetDlgItemText(hwndDlg, IDC_CFGNAME, cfgFileRelPath);
+
+						if (backupExists)
+							EnableWindow(GetDlgItem(hwndDlg, IDC_RESTORE), TRUE);
+						else
+							EnableWindow(GetDlgItem(hwndDlg, IDC_RESTORE), FALSE);
+					}
+					break;
+				}
+				case IDC_APPLY: {
+					DWORD patchResult = PatchExecutable(hwndDlg);
+
+					switch (patchResult) {
+						case UHC_PATCH_NONE:
+							MessageBoxW(hwndDlg, L"No patches have been applied to the EXE.", UHC_NAME, MB_ICONINFORMATION | MB_OK);
+							break;
+						case UHC_PATCH_FAILED:
+							if (CopyFileW(exeBackupPath, exePath, FALSE) && DeleteFileW(exeBackupPath))
+								MessageBoxW(hwndDlg, L"An error occurred while attempting to patch the EXE file\nThe original EXE file has been restored.", UHC_NAME, MB_ICONERROR | MB_OK);
+							else
+								MessageBoxW(hwndDlg, L"An error occurred while attempting to patch the EXE file\nThe patcher also could not restore the original EXE file", UHC_NAME, MB_ICONERROR | MB_OK);
+							break;
+						case UHC_PATCH_SUCCESS:
+							MessageBoxW(GetActiveWindow(), L"The EXE has been sucessfully patched!", UHC_NAME, MB_ICONINFORMATION | MB_OK);
+					}
+
+					break;
+
+				}
+				case IDC_AILIMIT:
+				case IDC_REVBANNERS:
+				case IDC_IGNOREREG:
+				case IDC_TEAMLIMIT: {
+					int chkBoxID = LOWORD(wParam);
+
+					if (IsDlgButtonChecked(hwndDlg, chkBoxID))
+						patchSettings |= ((DWORD) 1 << (DWORD) (chkBoxID - 1100));
+					else
+						patchSettings &= ~((DWORD) 1 << (DWORD) (chkBoxID - 1100));
+
+					int aux = 1;
+
+					break;
+				}
+
+			}
+			break;
+		default:
+			return FALSE;
+	}
+
+	return TRUE;
+}
+
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	LPWSTR *lpArgs;
+	int argCount;
+	BOOL exePathInitialized = FALSE;
+
+	lpArgs = CommandLineToArgvW(GetCommandLineW(), &argCount);
+
+	for (int i = 1; i < argCount; i++) {
+		switch (i) {
+		case 1:
+			lstrcpy(exePath, lpArgs[i]);
+			exePathInitialized = TRUE;
+			break;
+		case 2:
+			if (lstrlenW(lpArgs[i]) <= 20)
+				lstrcpyW(cfgFileRelPath, lpArgs[i]);
+			break;
+		}
+	}
+
+	if (!exePath) {
+		WCHAR relativeExePath[MAX_PATH];
+
+		GetCurrentDirectoryW(MAX_PATH, relativeExePath);
+		lstrcatW(relativeExePath, L"\\age3y.exe");
+
+		DWORD dwAttr = GetFileAttributesW(relativeExePath);
+
+		if ((dwAttr != INVALID_FILE_ATTRIBUTES) && !(dwAttr & FILE_ATTRIBUTE_DIRECTORY)) {
+			lstrcpy(exePath, relativeExePath);
+			exePathInitialized = true;
+		}
+	}
+
+	DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_MAIN), NULL, DialogProc, exePathInitialized);
+
+	return 0;
+}
---
+++ Patcher/Source/UHCPatcher/stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include "targetver.h"
+#include "Config.h"
+
+// Windows Header Files:
+#include <windows.h>
+#include <Strsafe.h>
+
+
+// TODO: reference additional headers your program requires here
